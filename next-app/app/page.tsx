81318769030d556091a1ae4930c4282b59803e6f
bctvizag <95524749+bctvizag@users.noreply.github.com>
2024-07-20 17:28:14 +0000

    project initiate

---
+++ next-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+   <>
+    <h1>hello nextjs</h1>
+    <Link href="users">Users</Link>
+  </>
+  )
+}
---
+++ next-app/app/users/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <div>
+        <div>UsersPage</div>
+        <Link href="new">New Userdd</Link> <br />
+        <Link href="/">Home</Link>
+    </div>
+  )
+}
+
+export default UsersPage
