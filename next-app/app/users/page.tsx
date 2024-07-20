8d2a200b23e5e84d3bbcafbd9d5e1d4acea27313
bctvizag <95524749+bctvizag@users.noreply.github.com>
2024-07-20 17:51:02 +0000

    datafetching from json placeholder

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,12 +1,29 @@
 import Link from 'next/link'
 import React from 'react'
 
-const UsersPage = () => {
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+const UsersPage = async() => {
+  const res = await fetch('http://jsonplaceholder.typicode.com/users');
+  const users: User[] = await res.json();
   return (
     <div>
         <div>UsersPage</div>
         <Link href="new">New Userdd</Link> <br />
         <Link href="/">Home</Link>
+
+        <h2 className='text-2xl font-bold mt-4 underline'>Users List</h2>
+
+        <ol>
+          {users.map(user => <li key={user.id}>{user.name}</li> )}
+        </ol>
+
+
     </div>
   )
 }
