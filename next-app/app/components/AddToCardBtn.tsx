6377664165edb75c486f9ebb256a97bcc8dd4ccd
bctvizag <95524749+bctvizag@users.noreply.github.com>
2024-07-21 12:05:14 +0000

    add daisyui

---
+++ next-app/app/components/AddToCardBtn.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+function AddToCardBtn() {
+  return (
+    <>
+      <button>Add to Cart</button>
+    </>
+  )
+}
+
+export default AddToCardBtn
--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import React from 'react'
 
+import ProductsCard from './comonents/ProductsCard'
+
 interface User {
   id: number;
   name: string;
@@ -8,22 +10,31 @@ interface User {
   email: string;
 }
 
-const UsersPage = async() => {
-  const res = await fetch('http://jsonplaceholder.typicode.com/users');
+const UsersPage = async () => {
+  const res = await fetch(
+    'http://jsonplaceholder.typicode.com/users',
+    { cache: 'no-store' }//this line used for disable caching    
+    /*   { next: { revalidate: 60 } }//this will make the page revalidate after 60 seconds */
+
+  );
   const users: User[] = await res.json();
   return (
     <div>
-        <div>UsersPage</div>
+      <div>UsersPage</div>
+      <nav>
         <Link href="new">New Userdd</Link> <br />
         <Link href="/">Home</Link>
-
-        <h2 className='text-2xl font-bold mt-4 underline'>Users List</h2>
-
-        <ol>
-          {users.map(user => <li key={user.id}>{user.name}</li> )}
-        </ol>
+      </nav>
+      <div>
+        <ProductsCard />
+      </div>
 
 
+      <h2 className='text-2xl font-bold mt-4 underline'>Users List</h2>
+
+      <ol>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ol>
     </div>
   )
 }
